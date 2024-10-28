9c41393ea26ee5478d33ea4b3d50165c758c5cfe
James Grieve <jamesonrgrieve@gmail.com>
2024-10-28 14:57:52 -0600

    Update network;

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,9 @@
 docker compose --profile dependencies -f docker-compose.yml -p pterodactyl down
-docker compose -f docker-compose-network.yml -p pterodactyl down
+docker compose -f docker-compose-network.yml -p pterodactyl-net down
 if [ ! -f ./.env ]; then
     cp ./.env.example ./.env
 fi
-docker compose -f docker-compose-network.yml -p pterodactyl pull
-docker compose -f docker-compose-network.yml -p pterodactyl up -d
+docker compose -f docker-compose-network.yml -p pterodactyl-net pull
+docker compose -f docker-compose-network.yml -p pterodactyl-net up -d
 docker compose --profile dependencies -f docker-compose.yml -p pterodactyl pull
 docker compose --profile dependencies -f docker-compose.yml -p pterodactyl up -d
