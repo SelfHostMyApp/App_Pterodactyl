0f92dbb5072d9f8dc40cbeaad90d859b90862394
James Grieve <jamesonrgrieve@gmail.com>
2024-10-28 14:44:42 -0600

    Reorder args;

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
-docker compose down
+docker compose --profile dependencies down
 if [ ! -f ./.env ]; then
     cp ./.env.example ./.env
 fi
-docker compose pull --profile dependencies
-docker compose up -d --profile dependencies
+docker compose --profile dependencies pull
+docker compose --profile dependencies up -d
